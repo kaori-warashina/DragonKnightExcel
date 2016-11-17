4e9a96bb4390506a536186fac6478f0e89ada0f0
Kaori Warashina <warashina@mustache-inc.co.jp>
2016-11-17 18:22:46 +0900

    no message

--- csv2json.js
+++ csv2json.js
@@ -8,15 +8,15 @@ fl.read(files, option , function (results){
     }
 });
 
-var fs = require('fs');
-var Converter = require("csvtojson").Converter;
-var converter = new Converter({});
+// var fs = require('fs');
+// var Converter = require("csvtojson").Converter;
+// var converter = new Converter({});
  
-//end_parsed will be emitted once parsing finished 
-converter.on("end_parsed", function (jsonArray) {
-	fs.writeFile( "master.json" , JSON.stringify(jsonArray, null, '    '));
-	console.log(jsonArray); //here is your result jsonarray 
-});
+// //end_parsed will be emitted once parsing finished 
+// converter.on("end_parsed", function (jsonArray) {
+// 	fs.writeFile( "master.json" , JSON.stringify(jsonArray, null, '    '));
+// 	console.log(jsonArray); //here is your result jsonarray 
+// });
 
-//read from file 
-require("fs").createReadStream( "character.csv" ).pipe(converter);
+// //read from file 
+// require("fs").createReadStream( "character.csv" ).pipe(converter);
