97aa8008bf5e9ea34db13e9693651a8890a99d01
Kaori Warashina <warashina@mustache-inc.co.jp>
2016-11-17 13:00:01 +0900

    no message

--- csv2json.js
+++ csv2json.js
@@ -1,16 +1,24 @@
-var fs = require('fs');
-var Converter = require("csvtojson").Converter;
-var converter = new Converter({});
+var fl      = require('node-filelist');
+var files   = [ "/" ];     //読み込みたいファイルディレクトリまたはパス(配列なので複数指定可)
+var option  = { "ext" : "csv" };   //読み込みたいファイルの拡張子(指定がない場合は全てのファイルを読み込みます)
 
-    converter.on("end_parsed", function (jsonArray) {
-        fs.writeFile('./master.json', JSON.stringify(jsonArray, null, '    '));
-        console.log("JSON形式で出力されました");
-    });
+//read csv file
+fl.read(files, option , function (results){
+    for(var i = 0; i < results.length; i++){
 
-var rs = fs.createReadStream('character.csv')
-    .pipe(iconv.decodeStream('SJIS'))
-    .pipe(iconv.encodeStream('UTF-8'))
-    .pipe(csv.parse())
-    .pipe(csv.transform(function(record) {console.log(record);}))
-    .pipe(converter)
-    ;
+    	//csv to json
+		var fs = require('fs');
+		var Converter = require("csvtojson").Converter;
+		var converter = new Converter({});
+		    converter.on("end_parsed", function (jsonArray) {
+		        fs.writeFile('./master.json', JSON.stringify(jsonArray, null, '    '));
+		        console.log("JSON形式で出力されました");
+		    });
+		    
+	//results csv to createReadStream
+	require("fs").createReadStream(results[i].path).pipe(converter);
+			
+	//csv file log	
+	console.log(results[i].path);
+    }
+});
