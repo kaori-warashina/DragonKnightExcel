c3e1aed7a1cc13c383eddb6daa94b97805326ed1
Kaori Warashina <warashina@mustache-inc.co.jp>
2016-11-17 15:54:21 +0900

    no message

--- csv2json.js
+++ csv2json.js
@@ -1,31 +1,20 @@
-var fl      = require('node-filelist');
-var files   = [ "/excel" ];     //読み込みたいファイルディレクトリまたはパス(配列なので複数指定可)
-var option  = { "ext" : "csv" };   //読み込みたいファイルの拡張子(指定がない場合は全てのファイルを読み込みます)
 
 // get file path 
-fl.read(files, option , function (results){
-    for(var i = 0; i < results.length; i++){
-    	var csvFile = results[i].path;
 
 
-	// // csv to json
+	// csv to json
 
-	// // file system  require
-	// var fs = require('fs');
-	// // converter require
-	// var Converter = require("csvtojson").Converter;
-	// var converter = new Converter({});
-	//     converter.on("end_parsed", function (jsonArray) {
-	//         fs.writeFile('./' + csvFile + '.json' , JSON.stringify(jsonArray, null, '    '));
-	//         console.log( csvFile + "JSON形式で出力されました");
-	//     });
+	// file system  require
+	var fs = require('fs');
+	// converter require
+	var Converter = require("csvtojson").Converter;
+	var converter = new Converter({});
+	    converter.on("end_parsed", function (jsonArray) {
+	        fs.writeFile( "master.json" , JSON.stringify(jsonArray, null, '    '));
+	        console.log( "JSON形式で出力されました");
+	    });
 
-	// // results csv to createReadStream
-	// require("fs").createReadStream(csvFile).pipe(converter);
-			
-
-
-      console.log(csvFile);
-    }
-});
-console.log(csvFile);
+	// results csv to createReadStream
+	require("fs").createReadStream(character0001.csv).pipe(converter);
+	require("fs").createReadStream(character のコピー 2.csv).pipe(converter);
+        }
