7f10884eb63373aeac4a9fe3471f2a7be78358c6
Kaori Warashina <warashina@mustache-inc.co.jp>
2016-11-17 10:44:34 +0900

    no message

--- csv2json.js
+++ csv2json.js
@@ -2,24 +2,10 @@ var fs = require('fs');
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
 
-
-var csvfile = require('fs');
-csvfile.readdir('.', function(err, files){
-    if (err) throw err;
-    var fileList = [];
-    files.filter(function(file){
-        return csvfile.statSync(file).isFile() && /.*\.csv$/.test(file); //絞り込み
-    }).forEach(function (file) {
-        fileList.push(file);
-    });
-    console.log(fileList);
-});
-
     converter.on("end_parsed", function (jsonArray) {
         fs.writeFile('./master.json', JSON.stringify(jsonArray, null, '    '));
         console.log("JSON形式で出力されました");
     });
  
-    require("fs").createReadStream(csvfile).pipe(converter);
-    // require("fs").createReadStream("character0001.csv").pipe(converter);
-
+    require("fs").createReadStream("character.csv").pipe(converter);
+    require("fs").createReadStream("character0001.csv").pipe(converter);
