26778286ea72c03721d2a6f44b82ba2f62bbf56b
Kaori Warashina <warashina@mustache-inc.co.jp>
2016-11-25 10:51:03 +0900

    Json 吐き出し先フォルダ指定

--- csv2json.js
+++ csv2json.js
@@ -3,14 +3,14 @@ fs.readdir('.', function(err, files){
     if (err) throw err;
     files.filter(function(file){
         //CSVのみを取得
-        return fs.statSync(file).isFile() && /.*\.csv$/.test(file); //絞り込み
+        return fs.statSync(file).isFile() && /.*\.csv$/.test(file);
     }).forEach(function (file) {
         //JSONに吐き出し
         var fs = require('fs');
         var Converter = require("csvtojson").Converter;
         var converter = new Converter({});
         converter.on("end_parsed", function (jsonArray) {
-            fs.writeFile(file +'.json', JSON.stringify(jsonArray, null, '    '));
+            fs.writeFile('/json/'+file +'.json', JSON.stringify(jsonArray, null, '    '));
             console.log(file + "JSON形式で出力されました");
         });
     require("fs").createReadStream( file ).pipe(converter);
